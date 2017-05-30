e3f26fdbfeee2050ef678d94d33ca421cc66be2f
AsuraPham <phamtuananh1202@gmail.com>
2017-05-30 18:31:13 +0700

    bang

---
+++ Prim/Prim/Source.cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <iostream>
+
+// số đỉnh của đồ thị
+#define  V 5
+using namespace std;
+// Một hàm tiện ích để tìm đỉnh với giá trị khóa cực tiểu từ
+// tập các đỉnh không có trong MST
+int minKey(int key[], bool mstSet[])
+{
+	// khởi tạo giá trị min
+	int min = INT_MAX; int min_index=0;
+	for (int v = 0; v < V;v++)
+	{
+		if (mstSet[v]==false&&key[v]<min)
+		{
+			min = key[v], min_index = v;
+		}
+		return min_index;
+	}
+}
+
+//Một chức năng tiện ích để in các MST xây dựng được lưu trữ trong parent[]
+int printMST(int parent[], int n, int graph[V][V])
+{
+	printf("Edge   Weight\n");
+	for (int i = 1; i < V; i++)
+		printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
+	return 0;
+}
+
+//Chức năng để xây dựng và in MST cho một biểu đồ được biểu diễn bằng sự kề nhau
+// biểu diễn ma trận
+void primMST(int graph[V][V])
+{
+	int parent[V]; //Mảng để lưu trữ được xây dựng MST
+	int key[V]; //Các giá trị chính được sử dụng để chọn cạnh trọng lượng tối thiểu trong cắt
+	bool mstSet[V]; //Để đại diện cho tập các đỉnh không có trong MST
+
+	//Khởi tạo tất cả các phím dưới dạng INFINITE
+	for (int i = 0; i < V;i++)
+	{
+		key[i] = INT_MAX, mstSet[i] = false;
+	}
+	// Luôn bao gồm đỉnh đầu tiên thứ nhất trong MST.
+	key[0] = 0; //Đặt phím 0 để đỉnh này được chọn là đỉnh đầu tiên
+	parent[0] = -1; //Nút đầu tiên luôn luôn là gốc của MST
+
+	//MST sẽ có đỉnh V
+	for (int count = 0; count < V - 1;count++)
+	{
+		// Chọn đỉnh quan trọng tối thiểu từ tập các đỉnh
+		//Chưa có trong MST
+		int u = minKey(key, mstSet);
+
+		// Thêm đỉnh đã chọn vào Bộ MST
+		mstSet[u] = true;
+
+		// Cập nhật giá trị khóa và chỉ mục cha của các đỉnh liền kề của
+		// Đỉnh được chọn.Xem xét chỉ những đỉnh chưa được
+		// Bao gồm trong MST
+		for (int v = 0; v < V; v++)
+		{
+			//graph[u][v] Không phải là không chỉ cho các đỉnh liền kề của m
+			//mstSet[v] Sai cho các đỉnh chưa có trong MST
+			//Cập nhật khóa chỉ khi graph[u][v] nhỏ hơn key [v]
+			if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
+				parent[v] = u, key[v] = graph[u][v];
+		}
+		// in cây mst 
+	}
+	printMST(parent, V, graph);
+}
+int main()
+{
+	int graph[V][V] = { { 0, 2, 0, 6, 0 },
+	{ 2, 0, 3, 8, 5 },
+	{ 0, 3, 0, 0, 7 },
+	{ 6, 8, 0, 0, 9 },
+	{ 0, 5, 7, 9, 0 },
+	};
+	primMST(graph);
+	return 0;
+}
+
---
+++ Prim/prim_V2/prim.cpp
@@ -0,0 +1,102 @@
+﻿#include < stdio.h > 
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define max 100
+
+typedef struct Egde {
+	int x, y;
+
+};
+
+//doc du lieu tu tap tin
+void Doc_File(int A[max][max], int & n) {
+	ifstream f;
+	f.open("input.txt", ios::in);
+	f >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) 
+		{
+			f >> A[i][j];
+		}
+	}
+	f.close();
+}
+
+//xuat du lieu ra tap tin
+void Ghi_File(Egde L[max], int n, int Sum) {
+	ofstream Output("output.txt");
+	Output << Sum<<"\n";
+	for (int i = 0; i < n - 1;i++)
+	{
+		Output << L[i].x + 1 << "-" << L[i].y + 1<<"\n";
+	}
+	Output.close();
+}
+
+//thuat toan Prim
+void Prim(int A[max][max], int n) {
+	char D[max];
+	Egde L[max];
+	int min = INT_MAX, Dem = 0, Sum = 0;
+	for (int i = 0; i < n; i++)
+		D[i] = 0;
+	for (int j = 1; j < n; j++)
+		if (min > A[0][j] && A[0][j] != 0) {
+			min = A[0][j];
+			L[0].y = j;
+		}
+	L[0].x = 0;
+	D[0] = 1;
+	D[L[0].y] = 1;
+	Sum += min;
+	Dem++;
+	do {
+		min = INT_MAX;
+		for (int i = 0; i < n; i++)
+			if (D[i] == 1) for (int j = 0; j < n; j++)
+				if (A[i][j] > 0 && min > A[i][j] && D[j] == 0) {
+					min = A[i][j];
+					L[Dem].x = i;
+					L[Dem].y = j;
+				}
+		Sum += min;
+		D[L[Dem].y] = 1;
+		Dem++;
+	} while (Dem < n - 1);
+	Ghi_File(L, n, Sum);
+}
+
+//chuong trinh chinh
+void main() {
+	int A[max][max], n;
+	Doc_File(A, n);
+	Prim(A, n);
+}
+/*
+Mô tả bài toán :
+
+Cho đồ thị vô hướng có trọng số G = (V, E) hãy tìm đường đi sao cho tất cả các đỉnh điều có đường đi với nhau và tổng trọng số của đường đi là nhỏ nhất.
+Tức là tìm đồ thị con liên thông G' của G sao cho tổng trọng số của G’ là nhỏ nhất.
+
+Ý tưởng thuật toán :
+
+Bước 1 : Xuất phát từ đỉnh k bất kỳ(thông thường chọn đỉnh đầu tiên) chọn một cạnh có trọng số nhỏ nhất liền kề với đỉnh k(min{ A[k][j] }j = 1..n) 
+ta đánh dấu 2 đỉnh đi qua cạnh đó và số cạnh tìm được là 1. Chuyển sang Bước 2.
+
+Bước 2 : Tìm cạnh nhỏ nhất của đồ thị với điều kiện cạnh tìm được phải có 1 đỉnh chưa đánh dấu và 1 đỉnh đã đánh dấu.Nghĩa là, ta tìm min{ A[i][j] }j = 1..n, i = 1..n 
+sao cho i đánh đấu và j chưa đánh dấu để tránh trường hợp tạo thành chu trình con.
+Ta tăng số cạnh tìm được lên 1 và chuyển sang Bước 3.
+
+Bước 3 : Nếu số cạnh tìm được bằng n - 1 kết thúc thuật toán, ngược lại quay về Bước 2.
+
+Mô tả dữ liệu đầu vào và đầu ra của bài toán :
+
++Dữ liệu vào : lưu trong tập tin Input.txt
+- Dòng đầu ghi số n là số đỉnh của một đồ thị(0 < n < 100)
+- Dòng i + 1 (1 < i < n) lưu ma trận kề của đồ thị với n số A[i, 1], A[i, 2]…A[i, n] mỗi số cách nhau bởi một khoảng trắng.
+
++ Dữ liệu ra : lưu trong file Output.txt
+- Dòng đầu ghi trọng số nhỏ nhất của cây bao trùm.
+- Các dòng còn lại lưu đường đi giữa đỉnh i nối với đỉnh j.*/
